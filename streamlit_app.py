544dd9661c1294d990179a9eb98c1ec479f33c1c
jewicy <josie.wuying@gmail.com>
2024-09-18 16:57:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ ingredients_string=''
 if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        st.subhead(fruit_chosen+' Nutrition Infomation')
+        st.subheader(fruit_chosen+' Nutrition Infomation')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_dataframe=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
